55ff427a4498be4fec586b35809b7e0c2a24f44f
Bernardo Marques <bernardohmarques@gmail.com>
2025-02-03 23:48:56 -0300

    add frontend architecture

--- apps/boxingrebel-frontend/src/app/app.component.ts
+++ apps/boxingrebel-frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  FaConfig,
+  FaIconComponent,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, FaIconComponent, NavbarComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'boxingrebel-frontend';
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ apps/boxingrebel-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,20 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faCartShopping,
+  faTruckFast,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faFacebook,
+  faTwitter,
+];
